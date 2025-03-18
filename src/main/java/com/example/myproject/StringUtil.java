6ef1f42423d1151a289cbc4b71d37bd945ec3204
Dmitriy Druzhkov <sedyar@gmail.com>
2025-03-18 14:44:15 +0300

    V2

--- src/main/java/com/example/myproject/StringUtil.java
+++ src/main/java/com/example/myproject/StringUtil.java
@@ -1,6 +1,13 @@
+package com.example.myproject;
+
 public class StringUtil {
 
     private StringUtil(){
+    }
+
+    public static void main(String[] args) {
+        String input = "J@va the be$t!123";
+        System.out.println(reverseString(input));
     }
 
     public static String reverseString(String input) {
